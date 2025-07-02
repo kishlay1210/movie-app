5008c1c3bae70b7ae96445457692406ef80bacd6
kishlay1210 <mr.kishlay@gmail.com>
2025-07-02 23:47:55 +0530

    MovieCard component with basic structure and favorite button added

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,27 @@
+import "../css/MovieCard.css"
+
+
+function MovieCard({movie}) {
+  
+
+    function onFavoriteClick() {
+        alert("Feature not implemented yet!");
+    }
+
+    return <div className="movie-card">
+        <div className="movie-poster">
+            <img src={movie.url} alt={movie.title}/>
+            <div className="movie-overlay">
+                <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavoriteClick}>
+                    ♥
+                </button>
+            </div>
+        </div>
+        <div className="movie-info">
+            <h3>{movie.title}</h3>
+            <p>{movie.release_date}</p>
+        </div>
+    </div>
+}
+
+export default MovieCard
