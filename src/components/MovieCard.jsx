79f100ba56b68dc876557978d1424621b6cfcaa9
kishlay1210 <mr.kishlay@gmail.com>
2025-07-03 17:14:30 +0530

    MovieCard component to implement favorite functionality and update image source

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,19 @@
 import "../css/MovieCard.css"
-
+import { useMovieContext } from "../contexts/MovieContext"
 
 function MovieCard({movie}) {
-  
+    const {isFavorite, addToFavorites, removeFromFavorites} = useMovieContext()
+    const favorite = isFavorite(movie.id)
 
-    function onFavoriteClick() {
-        alert("Feature not implemented yet!");
+    function onFavoriteClick(e) {
+        e.preventDefault()
+        if (favorite) removeFromFavorites(movie.id)
+        else addToFavorites(movie)
     }
 
     return <div className="movie-card">
         <div className="movie-poster">
-            <img src={movie.url} alt={movie.title}/>
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
             <div className="movie-overlay">
                 <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavoriteClick}>
                     ♥
@@ -19,7 +22,7 @@ function MovieCard({movie}) {
         </div>
         <div className="movie-info">
             <h3>{movie.title}</h3>
-            <p>{movie.release_date}</p>
+            <p>{movie.release_date?.split("-")[0]}</p>
         </div>
     </div>
 }
